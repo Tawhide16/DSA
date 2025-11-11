4f492ac1d287fbc482d9150cdac89e21bcc83509
Tawhide16 <tawideh.b10@gmail.com>
2025-11-11 11:35:31 +0600

    add all comment in stack

---
+++ stack-implement.js
@@ -0,0 +1,38 @@
+class stack {
+    constructor(){
+        this.items = []; // stack এর item গুলো রাখার জন্য array
+    }
+    //0(1)
+    push(value){
+        this.items.push(value); // নতুন item stack এর উপরে যোগ করা
+    }
+    //0(1)
+    pop(){
+        if(this.isEmpty()){
+             return undefined; // যদি stack খালি থাকে তাহলে undefined রিটার্ন করবে{
+        }
+            return this.items.pop(); // stack এর উপরের item টা রিমুভ করে রিটার্ন করবে
+    }
+    //0(1)
+    peek(){
+        if(this.isEmpty()){
+            return undefined; // যদি stack খালি থাকে তাহলে undefined রিটার্ন করবে
+        }
+        return this.items[this.items.length - 1]; // stack এর উপরের item টা রিটার্ন করবে
+    }
+    //0(1)
+    isEmpty(){
+        return this.items.length ===0 ; // stack খালি কিনা চেক করবে
+    }
+    //0(n)
+    print(){
+        console.log(this.items.slice().reverse().join("/")); // stack এর item গুলো উপরে থেকে নিচে পর্যন্ত দেখাবে
+    }
+}
+
+// stack class থেকে নতুন object তৈরি
+const myStack = new stack();
+myStack.push(10); // stack এ 10 যোগ করা
+myStack.push(20); // stack এ 20 যোগ করা
+myStack.push(30); // stack এ 30 যোগ করা
+myStack.print(); // Output: 30/20/10
