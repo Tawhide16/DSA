a0b1e57ebe45cfc94fac1373712b27c5ab68f2cc
Tawhide16 <tawideh.b10@gmail.com>
2025-11-11 10:37:19 +0600

    solve it man easy

--- lexical-enviroment.js
+++ lexical-enviroment.js
@@ -2,4 +2,20 @@ class counter {
     constructor(count) {
         this.count = count;
     }
+
+    add(amount) {
+    this.count = this.count + amount;
 }
+
+print(){
+    console.log(this.count)
+}
+
+}
+
+const myCounter = new counter(5);
+myCounter.add(3);
+myCounter.print()
+
+
+
