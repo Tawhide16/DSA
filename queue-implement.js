5d210bf8da837d5cc488c1897c68a779a25d757b
Tawhide16 <tawhideh.b10@gmail.com>
2025-11-12 09:03:46 +0600

    change the code

--- queue-implement.js
+++ queue-implement.js
@@ -4,7 +4,7 @@ class Queue {
     }
     //0(1)
     enqueue(value){
-        this.items.push(value); 
+        this.items.push(value); // নতুন item stack এর উপরে যোগ করা
     }
     //0(n)
     dequeue(){
