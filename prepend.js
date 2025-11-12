1870d02f26f992887781f18d58e2db12d25262b7
Tawhide16 <tawhideh.b10@gmail.com>
2025-11-12 12:19:10 +0600

    change the in prepend in remove

--- prepend.js
+++ prepend.js
@@ -118,7 +118,9 @@ class LinkedList {
     return this;
   }
 
-  
+  // ================================
+  // ❌ remove(index) — linked list থেকে node remove করা
+  // ================================
   remove(index) {
     // যদি ভুল index হয়
     if (index < 0 || index >= this.length) {
